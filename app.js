90002ab2f9224efa7087a0062ba08d8112fb8a59
pappjanos <papp.janos.antal@gmail.com>
2021-11-24 23:26:28 +0100

    register and login with jwt

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const db = require("./config/database.js");
 const User = require("./models/User");
 const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Op = Sequelize.Op;
+
+const JSON_SECRET_KEY = "secretkey";
 
 //test db
 db.authenticate()
@@ -15,35 +19,54 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/login", (req, res) => {
-  const token = req.body.email + req.body.password;
-  //res.json({ token });
-  res.send(400);
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({
+    where: { email },
+  });
+
+  bcrypt.compare(password, user.dataValues.password, (err, resolve) => {
+    if (err) {
+      console.log(err);
+    }
+    if (resolve) {
+      jwt.sign({ user: user.dataValues }, JSON_SECRET_KEY, {
+        expiresIn: "3000s",
+      }),
+        (err, token) => {
+          res.json({ success: true, user: token });
+        };
+    } else {
+      return res.json({
+        success: false,
+        status: "Password does not match",
+      });
+    }
+  });
 });
 
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
-  try {
-    const registeredEmail = await User.findOne({
-      where: { email },
-    });
+  const registeredEmail = await User.findOne({
+    where: { email },
+  });
 
-    if (registeredEmail) {
-      res.json({ registerStatus: "FAILED" });
-    } else {
-      try {
-        await User.create({
-          email,
-          password,
-        });
-      } catch (error) {
-        console.log(error);
+  if (registeredEmail) {
+    res.json({ success: false, status: "Already registered" });
+  } else {
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      let user = await User.create({
+        email,
+        password: hashedPassword,
+      });
+      if (user) {
+        res.json({ success: true, status: "Succesfull registration" });
+      } else {
+        res.json({ success: false, status: "Database error" });
       }
-      res.json({ registerStatus: "SUCCESS" });
-    }
-  } catch (error) {
-    console.log(error);
+    });
   }
 });
 
--- models/User.js
+++ models/User.js
@@ -1,13 +1,19 @@
 const Sequelize = require("sequelize");
 const db = require("../config/database");
 
-const User = db.define("user", {
-  email: {
-    type: Sequelize.STRING,
+const User = db.define(
+  "user",
+  {
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
   },
-  password: {
-    type: Sequelize.STRING,
-  },
-});
+  {
+    freezeTableName: true,
+  }
+);
 
 module.exports = User;
