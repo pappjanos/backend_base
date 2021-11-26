cd6c9470582f3b416ead6a09c804f1c80f1a2720
pappjanos <papp.janos.antal@gmail.com>
2021-11-26 16:34:55 +0100

    jwt

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Op = Sequelize.Op;
 
-const JSON_SECRET_KEY = "secretkey";
+const JWT_SECRET_KEY = "secretkey";
 
 //test db
 db.authenticate()
@@ -19,28 +19,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/restricted", verifyToken, (req, res) => {
+  jwt.verify(req.token, JWT_SECRET_KEY, (err, authData) =>
+    err ? res.sendStatus(403) : res.json({ message: "ok", authData })
+  );
+});
+
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({
     where: { email },
   });
+  if (!user) return res.json({ status: "fail", msg: "Not registered" });
 
   bcrypt.compare(password, user.dataValues.password, (err, resolve) => {
     if (err) {
       console.log(err);
     }
     if (resolve) {
-      jwt.sign({ user: user.dataValues }, JSON_SECRET_KEY, {
-        expiresIn: "3000s",
-      }),
+      jwt.sign(
+        { user: user.dataValues },
+        JWT_SECRET_KEY,
+        {
+          expiresIn: "3000s",
+        },
         (err, token) => {
-          res.json({ success: true, user: token });
-        };
+          return res.json({ status: "success", data: { token } });
+        }
+      );
     } else {
       return res.json({
-        success: false,
-        status: "Password does not match",
+        status: "fail",
+        msg: "Password does not match",
       });
     }
   });
@@ -54,21 +65,31 @@ app.post("/register", async (req, res) => {
   });
 
   if (registeredEmail) {
-    res.json({ success: false, status: "Already registered" });
-  } else {
-    bcrypt.hash(password, 10, async (err, hashedPassword) => {
-      let user = await User.create({
-        email,
-        password: hashedPassword,
-      });
-      if (user) {
-        res.json({ success: true, status: "Succesfull registration" });
-      } else {
-        res.json({ success: false, status: "Database error" });
-      }
-    });
+    return res.json({ status: "fail", msg: "Already registered" });
   }
+
+  bcrypt.hash(password, 10, async (err, hashedPassword) => {
+    let user = await User.create({
+      email,
+      password: hashedPassword,
+    });
+    return user
+      ? res.json({ status: "success", data: null })
+      : res.json({ status: "error", msg: "Database error" });
+  });
 });
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers.authorization;
+
+  if (typeof bearerHeader === "undefined") {
+    res.sendStatus(403);
+  } else {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  }
+}
 
 const PORT = process.env.PORT || 1111;
 app.listen(PORT, () => {
