f8a89bf7fac4683d644b1fc7088b1abdaad2b034
Janos Papp <papp.janos.antal@gmail.com>
2021-12-22 17:21:23 +0100

    del and patch added

--- app.js
+++ app.js
@@ -149,6 +149,25 @@ app.post("/blog-entry", async (req, res) => {
   }
 });
 
+app.delete("/blog-entry", async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id)
+      return res
+        .status(404)
+        .json({ message: "PostId is missing!", msg_id: "POST_ID_MISSING" });
+
+    await Blog.destroy({ where: { id }});
+    return res.status(200).json({
+      message: "Blog entry deleted succesfully!",
+      msg_id: "BLOG_ENTRY_DELETED",
+      id,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.get("/blog-entry", async (req, res) => {
   const { userId } = req.query;
   if (!userId)
@@ -164,6 +183,54 @@ app.get("/blog-entry", async (req, res) => {
 
   return res.status(200).json({ blogEntries });
 });
+
+
+app.patch("/blog-entry", async (req, res) => {
+  try {
+    const { id, title, text } = req.body;
+    if (!title && !text)
+      return res.status(500).json({
+        message: "Title and text is empty",
+        msg_id: "TITLE_AND_TEXT_EMPTY",
+      });
+
+    if (!id)
+      return res
+        .status(404)
+        .json({ message: "PostId is missing!", msg_id: "POST_ID_MISSING" });
+
+    const blogEntry = await Blog.findOne({ where: { id } });
+    if (!blogEntry)
+      return res.status(404).json({
+        message: "Blog entry not found",
+        msg_id: "BLOG_ENTRY_NOT_FOUND",
+      });
+
+    if (blogEntry) {
+      await Blog.update(
+        title ? {title} : text && {text}, {
+          where: {id}, 
+          returning: true,
+          plain: true
+      })
+
+      const updatedBlogEntry = await Blog.findOne({ where: { id } });
+
+      return res.status(200).json({
+        message: "Blog entry updated succesfully!",
+        msg_id: "BLOG_ENTRY_ADDED",
+        updatedBlogEntry,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      msg_id: "INTERNAL_SERVER_ERROR",
+    });
+  }
+});
+
 
 app.get("/blog-entry/:id", async (req, res) => {
   const postId = req.params.id;
